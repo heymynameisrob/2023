c837746cbe73956513ffa860777170d236e95ad6
Rob Hough <robhough@Robs-MacBook-Pro-2.local>
2022-11-13 19:39:37 +0000

    styles

--- static/main.js
+++ static/main.js
@@ -1,9 +1,21 @@
 function init(){
-    // sayHello();
+  setTheme();
+  toggleDarkMode();
 }
 
-function sayHello(){
-    alert('Hello');
+function toggleDarkMode() {    
+  const toggleTheme = document.getElementById('theme-toggle');
+
+  toggleTheme.addEventListener('click', function(e) {
+    let currentTheme = document.documentElement.getAttribute('data-theme');
+    currentTheme === 'light' ? document.documentElement.setAttribute('data-theme', 'dark') : document.documentElement.setAttribute('data-theme', 'light');
+  });
+}
+
+function setTheme(){
+  const doc = document.documentElement;
+  const pref = window.matchMedia('(prefers-color-scheme: dark)');
+  pref.matches ? doc.setAttribute('data-theme', 'dark') : doc.setAttribute('data-theme', 'light');
 }
 
 init();
