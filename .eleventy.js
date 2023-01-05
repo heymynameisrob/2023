a146fc315f0c62825921e65711f9f25ed465f855
Rob Hough <robhough@robs-mbp.home>
2023-01-05 19:03:26 +0000

    Improved Toggle, BG, and Spazzy link text

--- .eleventy.js
+++ .eleventy.js
@@ -6,40 +6,9 @@ const autoprefixer = require("autoprefixer");
 module.exports = function (eleventyConfig) {
 
   eleventyConfig.addPassthroughCopy("src/static");
-  eleventyConfig.addNunjucksAsyncFilter("jsmin", async function (
-    code,
-    callback
-  ) {
-    try {
-      const minified = await minify(code);
-      callback(null, minified.code);
-    } catch (err) {
-      console.error("Terser error: ", err);
-      // Fail gracefully.
-      callback(null, code);
-    }
-  });
-
-  eleventyConfig.addNunjucksAsyncFilter('postcss', (cssCode, done) => {
-    postCss([
-      autoprefixer(),
-      clean()
-    ])
-    .process(css, { from: undefined })
-    .then(
-      r => done(null, r.css),
-      e => done(e, null)
-    )
-  })
-
-  // eleventyConfig.addFilter("cssmin", function(code) {
-	// 	return new CleanCSS({
-  //     compatibility: '*',
-  //     format: 'beautify'
-  //   }
-  //   ).minify(code).styles;
-	// });
-
+  eleventyConfig.addWatchTarget('./src/styles.css');
+  eleventyConfig.addWatchTarget('./src/main.js');
+  
   return {
     templateFormats: [
       "md",
--- src/main.js
+++ src/main.js
@@ -2,6 +2,18 @@ function init(){
   setTheme();
   toggleDarkMode();
   getNowPlaying();
+  funkyLinkType();
+}
+
+function setThemeBackground(theme) {
+  const bgImg = document.querySelectorAll('.bg-fade--img')[0];
+  if( theme === 'dark' ) {
+    bgImg.src = './static/img/bg-dark.png';
+    bgImg.srcset = './static/img/bg-dark@2x.png 2x,'
+  } else {
+    bgImg.src = './static/img/bg-light.png';
+    bgImg.srcset = '/static/img/bg-light@2x.png 2x,'
+  }
 }
 
 function toggleDarkMode() {    
@@ -14,9 +26,13 @@ function toggleDarkMode() {
       if( currentTheme === 'dark' ) {
         document.documentElement.setAttribute('data-theme', 'light');
         localStorage.setItem('theme', 'light');
+        button.getElementsByTagName('p')[0].innerHTML = 'Lights on';
+        setThemeBackground('light');
       } else {
         document.documentElement.setAttribute('data-theme', 'dark');
         localStorage.setItem('theme', 'dark');
+        button.getElementsByTagName('p')[0].innerHTML = 'Lights off';
+        setThemeBackground('dark');
       }        
     });
     
@@ -28,12 +44,20 @@ function setTheme(){
   const pref = window.matchMedia('(prefers-color-scheme: dark)');
   const theme = localStorage.getItem('theme');
 
+  const button = document.querySelectorAll('[data-theme-toggle]')[1];
+
   if (theme === 'dark' || (theme === null && pref.matches)) {
     doc.setAttribute('data-theme', 'dark');
+    button.getElementsByTagName('p')[0].innerHTML = 'Lights off';
+    setThemeBackground('dark');
   } else {
     doc.setAttribute('data-theme', 'light');
+    button.getElementsByTagName('p')[0].innerHTML = 'Lights on';
+    setThemeBackground('light');
   }
 }
+
+
 
 function getNowPlaying() {
   fetch('https://hmnir-spotify-now-playing.onrender.com/')
@@ -54,5 +78,41 @@ function getNowPlaying() {
     `;
   });
 }
+
+function funkyLinkType () {  
+  const targetLink = document.querySelectorAll('a');
+  targetLink.forEach(link => {
+    const isExternalLink = link.href.includes('https://');
+    
+    if(!isExternalLink) return          
+
+    const randomDelay = () => {
+      const r = document.querySelector(':root');
+      const getRandomInt = () => { return Math.floor(Math.random() * 1000) + 1; };
+      r.style.setProperty('--random-delay', `${getRandomInt()}ms`);      
+    }    
+
+    let chars = link.innerHTML.split('');    
+    link.addEventListener('mouseenter', function(e) {      
+      link.innerHTML = '';            
+      randomDelay();
+
+      chars.forEach (char => { 
+        let span = document.createElement('span');
+        span.innerHTML = char;
+        link.append(span);
+        link.classList.add('spaz-out');
+      });            
+    });
+
+    link.addEventListener('mouseleave', function(e) {
+      link.innerHTML = '';
+      link.innerHTML = chars.join('');
+      link.classList.remove('spaz-out');
+    });
+
+  });
+}
+
 
 init();
